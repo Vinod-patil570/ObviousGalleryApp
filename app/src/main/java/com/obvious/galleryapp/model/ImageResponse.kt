90645d978ac65b8ab0ab35758b6c05dccb0d4aae
Vinod <vinodptl570@gmail.com>
2022-09-19 23:48:55 +0530

    Initial Commit
    Added required dependencies to gradle file
    Added raw "data.gson" file to assets folder
    Created Data classes for parsing Json file
    Enabled Hilt DI.

---
+++ app/src/main/java/com/obvious/galleryapp/model/ImageResponse.kt
@@ -0,0 +1,5 @@
+package com.example.obviousgalleryapp.model
+
+data class ImageResponse(
+    val imageResponse: List<ImageResponseItem?>? = null
+)
---
+++ app/src/main/java/com/obvious/galleryapp/model/ImageResponseItem.kt
@@ -0,0 +1,12 @@
+package com.example.obviousgalleryapp.model
+
+data class ImageResponseItem(
+	val date: String? = null,
+	val copyright: String? = null,
+	val mediaType: String? = null,
+	val hdurl: String? = null,
+	val serviceVersion: String? = null,
+	val explanation: String? = null,
+	val title: String? = null,
+	val url: String? = null
+)
