9c4842d5db5e2da071bac069879513cf7214a6a0
Vinod <vinodptl570@gmail.com>
2022-09-26 11:07:55 +0530

    Added Custom Progressbar for image loading.

--- app/src/main/java/com/obvious/galleryapp/adapter/FullScreenImageViewPagerAdapter.kt
+++ app/src/main/java/com/obvious/galleryapp/adapter/FullScreenImageViewPagerAdapter.kt
@@ -1,13 +1,19 @@
 package com.obvious.galleryapp.adapter
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import androidx.viewpager.widget.PagerAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.obviousgalleryapp.model.ImageResponseItem
 import com.obvious.galleryapp.R
 import java.util.*
@@ -33,7 +39,32 @@ class FullScreenImageViewPagerAdapter(
             context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater?
         val view = layoutInflater?.inflate(R.layout.image_vp_item, container, false)
         val imageView: ImageView = view?.findViewById<View>(R.id.vpImageView) as ImageView
-        Glide.with(context).load(imageList.get(position).url).centerCrop().into(imageView)
+        val progressBar: ProgressBar = view?.findViewById<View>(R.id.progressbar) as ProgressBar
+        Glide.with(context).load(imageList.get(position).url)
+            .addListener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    progressBar.visibility = View.GONE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    progressBar.visibility = View.GONE
+                    return false
+                }
+
+            }
+            ).centerCrop().into(imageView)
         Objects.requireNonNull(container).addView(view)
 
 
